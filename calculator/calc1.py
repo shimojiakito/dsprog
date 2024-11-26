76c5f93db6e6bc960fefc1931bc20859f2282be2
Shimoji Akito <s2422091@stu.musashino-u.ac.jp>
2024-11-26 23:33:56 +0900

    calc1.pyを追加し編集

---
+++ calculator/calc1.py
@@ -0,0 +1,39 @@
+import flet as ft
+import math
+
+
+def main(page: ft.Page):
+    page.title = "Calc App"
+    result = ft.Text(value="0")
+
+    page.add(
+        result,
+        ft.ElevatedButton(text="1/x"),
+        ft.ElevatedButton(text="x!"),
+        ft.ElevatedButton(text="x²"),
+        ft.ElevatedButton(text="x³"),
+        ft.ElevatedButton(text="10^x"),
+        ft.ElevatedButton(text="AC"),
+        ft.ElevatedButton(text="+/-"),
+        ft.ElevatedButton(text="%"),
+        ft.ElevatedButton(text="/"),
+        ft.ElevatedButton(text="7"),
+        ft.ElevatedButton(text="8"),
+        ft.ElevatedButton(text="9"),
+        ft.ElevatedButton(text="*"),
+        ft.ElevatedButton(text="4"),
+        ft.ElevatedButton(text="5"),
+        ft.ElevatedButton(text="6"),
+        ft.ElevatedButton(text="-"),
+        ft.ElevatedButton(text="1"),
+        ft.ElevatedButton(text="2"),
+        ft.ElevatedButton(text="3"),
+        ft.ElevatedButton(text="+"),
+        ft.ElevatedButton(text="0"),
+        ft.ElevatedButton(text="."),
+        ft.ElevatedButton(text="="),
+    )
+
+
+ft.app(target=main)
+
